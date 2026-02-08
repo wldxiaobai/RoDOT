859742273a76ab283beb304b39ad8b5b73766d8a
wldxiaobai <944418001@qq.com>
2026-02-08 21:34:47 +0800

    add start menu

---
+++ Assets/Scripts/GlobalControl/Save&Load.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int score;
+    public float playerX;
+    public float playerY;
+    // 你需要保存的数据
+}
+
+public class SaveManeger : Globalizer<SaveManeger>
+{
+    public void SaveGame(SaveData data)
+    {
+        string json = JsonUtility.ToJson(data);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+    }
+
+    public SaveData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/save.json";
+        if (System.IO.File.Exists(path))
+        {
+            string json = System.IO.File.ReadAllText(path);
+            return JsonUtility.FromJson<SaveData>(json);
+        }
+        return null;
+    }
+}
